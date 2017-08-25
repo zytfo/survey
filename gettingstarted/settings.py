25988bb7d0a055a84a4e39bd0d637a2539b9d740
Ilya Borovik <ilyaborovik@MacBook-Pro-Ilya.local>
2017-08-26 00:07:32 +0300

    PostForm

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'gettingstarted.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(SETTINGS_PATH, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'debug': True,
