51f441a0adbc815b14f0bb70b3804b252e5a29fb
Ilya Borovik <ilyaborovik@MacBook-Pro-Ilya.local>
2017-08-26 00:14:11 +0300

    Form Testing

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     url(r'^db', hello.views.db, name='db'),
     url(r'^survey', hello.views.survey, name='survey'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^post/form_upload.html$',
+    url(r'^post_form.html$',
         hello.views.post_form_upload, name='post_form_upload'),
 ]
