bdac79d0351b7c03c6a82298f7899e049cd5de88
Ilya Borovik <ilyaborovik@MacBook-Pro-Ilya.local>
2017-08-26 21:22:17 +0300

    Adapter Fix

--- hello/dbadapter.py
+++ hello/dbadapter.py
@@ -23,15 +23,30 @@ class DBAdapter:
     def insert_answers(self, survey_id, *answers):
         """ Добавляем ответы в бд """
         with self.connection:
-            question1 = [False, False, False, False, False, False]
+            question1 = [False for i in range(8)]
             for i in answers[0]:
                 question1[int(i)] = True;
-            self.cursor.execute('INSERT INTO question_1(survey_id, choice_1, choice_2, choice_3, choice_4, choice_5, choice_6)'
-                ' VALUES(%s, %s, %s, %s, %s, %s, %s)', (survey_id, *question1))
+            self.cursor.execute('INSERT INTO question_1(survey_id, choice_1, choice_2, choice_3, choice_4, choice_5, choice_6, choice_7, choice_8)'
+                ' VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)', (survey_id, *question1))
             self.cursor.execute('INSERT INTO question_2(survey_id, level) VALUES(%s, %s)', (survey_id, answers[1]))
             self.cursor.execute('INSERT INTO question_3(survey_id, choice_id) VALUES(%s, %s)', (survey_id, answers[2]))
             self.cursor.execute('INSERT INTO question_4(survey_id, choice_id) VALUES(%s, %s)', (survey_id, answers[3]))
             self.cursor.execute('INSERT INTO question_5(survey_id, text) VALUES(%s, %s)', (survey_id, answers[4]))
+
+    def get_results(self):
+        ''' Получаем данные опроса '''
+        with self.connection:
+            self.cursor.execute('SELECT * FROM question_1 ORDER BY survey_id')
+            question_1 = self.cursor.fetchall()
+            self.cursor.execute('SELECT * FROM question_2 ORDER BY survey_id')
+            question_2 = self.cursor.fetchall()
+            self.cursor.execute('SELECT * FROM question_3 ORDER BY survey_id')
+            question_3 = self.cursor.fetchall()
+            self.cursor.execute('SELECT * FROM question_4 ORDER BY survey_id')
+            question_4 = self.cursor.fetchall()
+            self.cursor.execute('SELECT * FROM question_5 ORDER BY survey_id')
+            question_5 = self.cursor.fetchall()
+            return [question1, question_2, question_3, question_4, question_5]
 
     def get_datetime(self):
         with self.connection:
