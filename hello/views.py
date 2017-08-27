3052439eb72fe8bcfea87a9720713aa5ed3b81e9
borisqa <sandman96@mail.ru>
2017-08-27 16:51:38 +0300

    added Test Auth

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.template.context_processors import csrf
+from django.shortcuts import render_to_response, redirect
 from .models import Greeting
 from .forms import SurveyForm, SurveyFormRu
 from .dbadapter import DBAdapter
@@ -67,6 +71,7 @@ def survey_ru(request):
     return render(request, 'survey_ru.html', {
         'form': form}, RequestContext(request))
 
+@login_required(login_url='/login/')
 def results(request):
     db = DBAdapter()
     results = db.get_results()
@@ -83,3 +88,18 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
+
+def auth(request):
+    user = authenticate(username=request.POST['username'], password=request.POST['password'])
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect('/en/results/')
+    else: 
+        return HttpResponseRedirect('/login/')
+
+def loginview(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('login.html', c)
+
+
