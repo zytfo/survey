7eedb8795d45479e13d6fa5f14e756ce8e02ad00
Artur Khayaliev <a.khayaliev@innopolis.ru>
2017-09-11 10:50:25 +0300

    professor's check

--- hello/forms.py
+++ hello/forms.py
@@ -3,20 +3,20 @@ from django.contrib.auth import authenticate
 from .localizations import Localization as locale
 
 class SurveyForm(forms.Form):
-    question1 = forms.MultipleChoiceField(choices=locale.survey_form['en']['question_1_choices'], widget=forms.CheckboxSelectMultiple())
+    #question1 = forms.MultipleChoiceField(choices=locale.survey_form['en']['question_1_choices'], widget=forms.CheckboxSelectMultiple())
     question2 = forms.ChoiceField(choices=locale.survey_form['all']['question_2_choices'], widget=forms.Select(attrs={'class': 'form-control', 'id': 'selectbasic', 'name': 'selectbasic'}))
     question3 = forms.ChoiceField(required=True, choices=locale.survey_form['en']['question_3_choices'], widget=forms.RadioSelect(), initial='0')
     question4 = forms.ChoiceField(required=True, choices=locale.survey_form['all']['question_4_choices'], widget=forms.RadioSelect(attrs={'name': 'radios_age'}), initial='0')
     question5 = forms.CharField(min_length=256, widget=forms.Textarea(attrs={'class': 'form-control', 'id': 'textarea', 'name': 'textarea'}))
-    # question6 = forms.MultipleChoiceField(choices=locale.survey_form['all']['question_6_choices'], widget=forms.CheckboxSelectMultiple())
+    question6 = forms.MultipleChoiceField(choices=locale.survey_form['all']['question_6_choices'], widget=forms.CheckboxSelectMultiple())
 
 class SurveyFormRu(forms.Form):
-    question1 = forms.MultipleChoiceField(choices=locale.survey_form['ru']['question_1_choices'], widget=forms.CheckboxSelectMultiple())
+    #question1 = forms.MultipleChoiceField(choices=locale.survey_form['ru']['question_1_choices'], widget=forms.CheckboxSelectMultiple())
     question2 = forms.ChoiceField(choices=locale.survey_form['all']['question_2_choices'], widget=forms.Select(attrs={'class': 'form-control', 'id': 'selectbasic', 'name': 'selectbasic'}))
     question3 = forms.ChoiceField(required=True, choices=locale.survey_form['ru']['question_3_choices'], widget=forms.RadioSelect(), initial='0')
     question4 = forms.ChoiceField(required=True, choices=locale.survey_form['all']['question_4_choices'], widget=forms.RadioSelect(attrs={'name': 'radios_age'}), initial='0')
     question5 = forms.CharField(min_length=256, widget=forms.Textarea(attrs={'class': 'form-control', 'id': 'textarea', 'name': 'textarea'}))
-    # question6 = forms.MultipleChoiceField(choices=locale.survey_form['all']['question_6_choices'], widget=forms.CheckboxSelectMultiple())
+    question6 = forms.MultipleChoiceField(choices=locale.survey_form['all']['question_6_choices'], widget=forms.CheckboxSelectMultiple())
 
 class LoginForm(forms.Form):
   username = forms.CharField(max_length=255, required=True)
